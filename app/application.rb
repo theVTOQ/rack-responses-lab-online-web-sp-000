8b671f4c78432bf279e3ff76916c8a065e9dc50b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-28 19:20:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
     resp = Rack::Response.new
     time = Time.new
     if time.hour < 12
-      resp.write "Good Morning!"
-    else
       resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
     end
     resp.finish
   end
